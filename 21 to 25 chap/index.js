248539aa4dbc095a5a33fd96d7267989374616b5
subhanrizwan0 <subhanahmed444sherazi@gmail.com>
2022-07-15 09:21:51 +0500

    java script

---
+++ 21 to 25 chap/index.js
@@ -0,0 +1,111 @@
+// question 1
+// var first = "subhan"
+// var sec = "ahmed"
+// var fullname = "subhan" + " ahmed"
+// console.log(fullname);
+
+// question 2
+// var mobile = prompt("enter the mobile phone model")
+// console.log(mobile.length);
+
+// question 3
+// var count = "turkish"
+// console.log(count.indexOf("k"));
+
+
+//question 4
+// var text = "hellow world"
+// console.log(text.LastindexOf("l"));
+
+
+// question 5
+// var mulk = "pakistani"
+// console.log(mulk.indexOf("i"));
+
+// question 6
+// var first = "subhan"
+// var sec = " ahmed"
+// var fullname = first.concat(sec)
+// console.log(fullname); 
+
+
+// question 7
+// var mulk = "hyderabad"
+// mulk = mulk.replace("hyderabad","islamabad") 
+// console.log(mulk);
+
+// question 8
+// var text = "Ali and Sami are best friends.and They play cricket football together"
+// console.log(text.replace(/and/g , "&"));
+
+// question 9
+
+// var num = "472"
+// console.log(num);
+
+// var num = "472"
+// console.log(Number(num));
+
+// var num = "472"
+// console.log(parseInt(num));
+
+// var num = "472"
+// console.log(parseFloat(num));
+
+// var num = + "472"
+// console.log(num);
+
+// question 10
+// var fd = "peanut"
+// console.log(fd);
+// fd1 = fd.toUpperCase("")
+// console.log(fd1);
+
+// question 11
+// var js =prompt("enter")
+// var js1 = js.slice(0,1)
+// js1=js1.toUpperCase("")
+//  var js2 = js.slice(1)
+// js2=js2.toLowerCase("")
+// var finl = js1 + js2
+// console.log(js);
+// console.log(finl);
+
+// question 12
+// var num = "35.36"
+// console.log(num.replace(".",""));
+
+// question 13 
+
+// question 14
+// var foods = ["cake, apple pie, cookie, chips, patties"]
+// var fd = prompt("enter")
+// var flag = false
+// for(var i =0; i < foods.length; i++ ){
+//     if(fd === foods[i]){
+//         flag=true
+      
+//     }
+// }if(flag===""){
+//     alert(" available")
+// }else{alert(" not available")}
+
+// question 15
+
+
+// question 16
+// var uni = "university o karachi"
+// console.log(uni.split(""));
+
+// question 17
+// var text = "pakistan"
+// console.log(text.slice(7));
+
+// question 18
+// var text =  "The quick brown fox jumps over the lazy dog"
+// count = 0
+// for(var i = 0; i<text.length; i++){
+//     if (text.slice(i,i+3)==="the"){
+//         count++;
+//     }console.log(count);
+//     }
